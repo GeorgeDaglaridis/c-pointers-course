7fe207b5954de64bd3a01e99f7885be5e332b901
George Theodoros P. Daglaridis <george_daglaridis@yahoo.com>
2026-03-18 13:30:41 +0200

    start checking pointers

---
+++ pointers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+
+    char c = 'A';
+    char *c_ptr = &c;
+
+    printf("c = %c, &c = %p\n", c, &c);
+    printf("*c_ptr = %c, c_ptr = %p\n", *c_ptr, c_ptr);
+    printf("&c_ptr = %p\n", &c_ptr);
+
+    return 0;
+}
