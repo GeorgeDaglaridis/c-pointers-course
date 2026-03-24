ce2edad171f1a50f41f66bbffc6e3fc6e34a9c3a
George Theodoros P. Daglaridis <george_daglaridis@yahoo.com>
2026-03-24 15:48:06 +0200

    add 4_1.c-4_3.c, 5_2.c-5_4.c, 6_1.c-6_2.c, 7_1.c-7_2.c

---
+++ material_from_piyush/5-4.c
@@ -0,0 +1,38 @@
+// math_library.h
+#define ADD 0
+#define SUB 1
+#define MUL 2
+#define DIV 3
+
+void execute_operation(int operation, int a, int b);
+
+// math_library.c
+#include "math_library.h"
+#include <stdio.h>
+
+static void add(int x, int y) { printf("add: %d\n", x + y); }
+
+static void sub(int x, int y) { printf("sub: %d\n", x - y); }
+
+static void mul(int x, int y) { printf("mul: %d\n", x * y); }
+
+static void div(int x, int y) { printf("div: %d\n", x / y); }
+
+typedef void (*func_t)(int a, int b);
+
+func_t func_lib[4] = {add, sub, mul, div};
+
+void execute_operation(int operation, int a, int b) {
+  func_lib[operation](a, b);
+}
+
+// main.c
+#include "math_library.h"
+
+int main() {
+  execute_operation(ADD, 100, 5);
+  execute_operation(SUB, 100, 5);
+  execute_operation(MUL, 100, 5);
+  execute_operation(DIV, 100, 5);
+  return 0;
+}
